aa5df0fbc916f07c268064753f2516d3032749a3
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-07-28 10:51:11 +0300

    instanciate order.py

--- server/models/order.py
+++ server/models/order.py
@@ -1,4 +1,5 @@
 class Order(db.Model):
+    __tablename__ = 'order'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
