e7db300b19e319b7ac7b230eba43b1e0befe49c6
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-07-28 10:53:18 +0300

    complete service.py

--- server/models/service.py
+++ server/models/service.py
@@ -1,4 +1,6 @@
 class Service(db.Model):
+    __tablename__ = 'service'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.Text)
