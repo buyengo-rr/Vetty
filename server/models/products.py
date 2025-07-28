5bbc2b9e7a8a18aba8b86fdc9a8fc985e180b7a1
ian <ianwisdom90@gmail.com>
2025-07-28 10:07:29 +0300

    products

--- server/models/products.py
+++ server/models/products.py
@@ -8,6 +8,7 @@ class Product(db.Model):
     category = db.Column(db.String) 
 
     order_items = db.relationship('OrderItem', back_populates='product')
+    
 
     def __repr__(self):
         return f"<Product {self.name} (ID: {self.id}) - ${self.price}>"
